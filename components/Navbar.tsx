7fff053f2d4cdeb1b00ba1ef25f9140ed293a2b1
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-06-28 12:57:38 +0530

    added theme and navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,35 @@
-import AppBar from "@material-ui/core/AppBar"
+import {ReactElement, cloneElement} from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
 
 interface Props {
-	
+  children: ReactElement;
 }
 
-const Navbar = (props: Props) => {
-	return (
-		<AppBar></AppBar>
-	)
+function ElevationAppBar(props: Props) {
+  const { children } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
 }
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <ElevationAppBar >
+      <AppBar position="fixed" color="secondary">
+				<Toolbar />
+			</AppBar>
+    </ElevationAppBar>
+  );
+};
+
+export default Navbar;
---
+++ styles/index.ts
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: "#14B8A6",
+      light: "#2DD4BF",
+			dark: '#0D9488'
+    },
+		secondary: {
+			main: '#06B6D4'
+		}
+  },
+});

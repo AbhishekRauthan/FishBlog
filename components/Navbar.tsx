e133892b0501a3814add3a1330ee3786be19dee8
Abhishek Rauthan <abhishek.rauthan.10@gmail.com>
2021-09-27 12:33:47 +0530

    modified Navbar and added Layput component. Replaced styled-component with emotion for performance

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import tw, { styled } from "twin.macro";
 
-const Nav = tw.header``;
+const NavButton = styled.button(() => [
+  tw`h-16 w-16 rounded-full shadow-lg bg-gray-100 outline-none`
+]);
 
 const Navbar = () => {
-  return <Nav></Nav>;
+  const [isClosed, setIsClosed] = useState(true)
+  return (
+    <>
+      <header tw="fixed z-40 w-full bg-transparent">
+        <nav tw="flex justify-end items-center container py-3 px-5 mx-auto">
+          <NavButton onClick={()=> setIsClosed(!isClosed)}></NavButton>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default Navbar;
